746a70dbeeffdb731731f6887cf3da3525bcd612
Chris Roberts <chrisroberts.code@gmail.com>
2014-08-19 14:04:24 -0700

    Add production and permission registration

--- lib/fission-app-woodchuck/engine.rb
+++ lib/fission-app-woodchuck/engine.rb
@@ -1,6 +1,19 @@
 module FissonApp
   module Woodchuck
     class Engine < ::Rails::Engine
+
+      # Ensure woodchuck is registered as a valid product. Register
+      # permission to allow access.
+      config.to_prepare do |config|
+        product = Fission::Data::Models::Product.find_or_create(:name => 'woodchuck')
+        unless(product.permissions_dataset.where(:name => 'woodchuck_base').count > 0)
+          product.add_permission(
+            :name => 'woodchuck_base',
+            :pattern => '/woodchuck.*'
+          )
+        end
+      end
+
     end
   end
 end
