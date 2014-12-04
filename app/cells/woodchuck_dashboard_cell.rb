e1598c7b1d87cfc6dba3b6454a37a7bd2b6f4e32
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-04 12:47:24 -0800

    Use categorized chart for display

--- app/cells/woodchuck_dashboard_cell.rb
+++ app/cells/woodchuck_dashboard_cell.rb
@@ -1,12 +1,14 @@
-class WoodchuckDashboardCell < Cell::Rails
+class WoodchuckDashboardCell < DashboardCell
 
   def show(args)
+    super
     #fetch log metadata
-    log_data = LogEntry.eager_graph(:log => :account).where(:account_id => 1).
-      where{log_entries__created_at >= (1.day.ago)}.group_and_count{
+    log_data = LogEntry.eager_graph(:log => :account).where(:account_id => current_user.run_state.current_account.id).
+      where{log_entries__created_at >= 4.hours.ago.to_time}.group_and_count{
       date_trunc('hour', log_entries__created_at)
     }.order_by(:date_trunc).to_a
-    @logs = [log_data.map{|l| l[:date_trunc].strftime("%Y-%m-%d %H")}.unshift('x'), log_data.map{|l| l[:count]}.unshift('data')]
+    @categories = log_data.map{|l| l[:date_trunc].strftime('%H:%M')}
+    @logs = [log_data.map{|l| l[:count]}.unshift('data')]
     render
   end
 
